3777b5040256166d86b105e6acca954288718243
MohammadRezaL <mlalegany1@yahoo.com>
2026-06-29 14:23:14 +0330

    Initial commit

---
+++ app/src/main/java/com/example/mathalarm/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.mathalarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MathAlarmApp()
+        }
+    }
+}
+
+@Composable
+fun MathAlarmApp() {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Math Alarm")
+            }
+        }
+    }
+}
